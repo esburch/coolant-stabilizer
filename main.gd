c7dda7db86e70f8a61186b107ac2049b7b970c83
Emily Burch <burch.emilys@gmail.com>
2024-07-05 17:50:06 -0400

    Add progress bar

--- main.gd
+++ main.gd
@@ -3,11 +3,14 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+onready var timer = get_node("Coolant/Timer")
+onready var pb = get_node("TextureProgress")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	pb.value=5
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	print(timer.time_left)
+	pb.value = 5-timer.time_left
